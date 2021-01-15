1c1b5e8adb91afb4f9589443e4cc841ad4cc5583
dwisecar <dwisecar@gmail.com>
2021-01-15 12:57:33 -0800

    commit tree 4f07a49fd919f4d63d403563e26b91499a029867

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+// Write your code here 
+const num1 = 2;
+const num2 = 31;
+const multiply = (num1 * num2);
+const random = Math.random() + 1;
+const mod = 9 % 5;
+const max = Math.max(1,20);
+
+
